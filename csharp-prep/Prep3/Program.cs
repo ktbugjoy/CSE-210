7357da1b9f524f2af47d24bf7b8bc5e30e4ddea7
ktbugjoy <bro21001@byui.edu>
2024-05-02 22:56:40 -0600

    Magic Number Game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,27 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+class Program {
+    static void Main() {
+        Console.Write("What is the magic number? ");
+        int magicNumber = Convert.ToInt32(Console.ReadLine());
+
+        // Initialize the guess to a value
+        int guess = 0;
+
+        // Loop until the guess is correct
+        while (guess != magicNumber) {
+            // Ask the user for a guess
+            Console.Write("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            // Determine the response using if statements
+            if (guess < magicNumber) {
+                Console.WriteLine("Higher");
+            } else if (guess > magicNumber) {
+                Console.WriteLine("Lower");
+            }
+        }
+        // The correct guess exits the loop, so we print the success message here
+        Console.WriteLine("You guessed it!");
     }
 }
